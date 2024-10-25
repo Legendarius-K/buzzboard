7cf72e2005c5898bf77ee2ebc15047ecb4baf26c
Darius K <darius.kaya@gmail.com>
2024-10-25 09:21:40 +0200

    added time distance

--- actions/schemas.ts
+++ actions/schemas.ts
@@ -2,5 +2,6 @@ import { z } from 'zod'
 
 export const logInSchema = z.object({
     email: z.string().email(),
+    // username: z.string().min(4, 'Username must be 4-15 characters').max(15, 'Username must be 4-15 characters'),
     password: z.string().min(5, "Password must be at least 5 characters")
 })
--- src/components/post.tsx
+++ src/components/post.tsx
@@ -1,7 +1,7 @@
 import Link from "next/link";
-import { Database } from "../../utils/supabase/database.types";
 import { ChevronDown, User } from "lucide-react";
 import { cn } from "@/lib/utils";
+import { formatDistanceToNow } from 'date-fns';
 
 export const Post = ({
     id,
@@ -21,26 +21,33 @@ export const Post = ({
     category: string
 }) => {
 
+    // Format the date to show relative time
+    const timeAgo = formatDistanceToNow(new Date(date), { addSuffix: true });
+
     return (
         <div className="flex items-center gap-3 w-full md:w-auto relative">
-            <p className="text-neutral-500 w-[70px] text-[12px]">{date.slice(2, 10)}</p>
-            <div className=" bg-neutral-200  transition-all p-3 pb-0 w-full  md:w-[450px] rounded-xl flex flex-col items-center">
+            <p className="text-neutral-500 w-[70px] text-[12px]">{timeAgo}</p>
+            <div className="bg-neutral-200 transition-all p-3 pb-0 w-full md:w-[450px] rounded-xl flex flex-col items-center">
                 <div className="w-full relative flex justify-between pb-1">
-                    <h2 className=" text-sm font-medium flex gap-1"><User size={18} />{author}</h2>
+                    <h2 className="text-sm font-medium flex gap-1"><User size={18} />{author}</h2>
                     <h3 className="font-medium">{title}</h3>
                     <Link
                         href={`/category/${category}`}
                         className={cn("text-sm hover:text-blue-900 hover:underline font-medium",
                             category === 'coding' && 'text-amber-800',
                             category === 'general' && 'text-green-800'
-                        )}>{category}</Link>
+                        )}
+                    >
+                        {category}
+                    </Link>
                 </div>
                 <div className="w-full text-sm p-1 bg-neutral-50 rounded-lg h-14 overflow-hidden">
                     {content}
                 </div>
-                <Link className=" transition-all w-full rounded-lg" href={`/single-post/${slug}`}><ChevronDown className="group w-full" color="gray" /></Link>
+                <Link className="transition-all w-full rounded-lg" href={`/single-post/${slug}`}>
+                    <ChevronDown className="group w-full" color="gray" />
+                </Link>
             </div>
         </div>
     )
 };
-
